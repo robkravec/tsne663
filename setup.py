ca6b5afe8bf729effe8d8c241d19d9cbb9052b07
Rob Kravec <rkravec17@gmail.com>
2021-04-22 20:49:18 +0000

    Fix setup

--- setup.py
+++ setup.py
@@ -8,5 +8,5 @@ setup(name='tsne663',
       author_email='marc.brooks@duke.edu, robert.kravec@duke.edu, steven.winter@duke.edu',
       license='MIT',
       packages=find_packages(), 
-      install_requires = ['matplotlib', 'numpy', 'numba', 'tqdm', 'sklearn'],
+      install_requires = ['matplotlib', 'numpy', 'numba', 'tqdm', 'scikit-learn'],
       py_modules = ['tsne', 'sim'])
